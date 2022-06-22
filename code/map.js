73beb77f9ee268d8b9be40c6980e1695e7a031bf
Shannon Watts <100214297+Shannon-Watts@users.noreply.github.com>
2022-06-22 18:33:15 +0100

    started leaflet code

---
+++ code/map.js
@@ -0,0 +1,14 @@
+// Creating the map object
+var myMap = L.map("my_map", {
+    center: [50.45, 30.52],
+    zoom: 6
+  });
+  
+// Adding the tile layer
+L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+}).addTo(myMap);
+
+
+
+
