739e65261a47ac577dc2d60804a336a7d61d10e6
Shannon Watts <100214297+Shannon-Watts@users.noreply.github.com>
2022-06-22 18:44:19 +0100

    working on code - waiting for json

--- code/map.js
+++ code/map.js
@@ -9,6 +9,21 @@ L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
     attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
 }).addTo(myMap);
 
+// Kiev city marker 
+var marker = L.marker([50.45, 30.5], {
+    draggable: false,
+    title: "Kiev"
+}).addTo(myMap);
+marker.binddPopup("Kiev, Ukraine")
 
+// add in json and add markers to capital cities 
 
+// add in json and create 2 layers 
+
+// count of event type civilian violence 
+
+// count of fatalities 
+
+// Layer control 
+// L.control.layers()
 
