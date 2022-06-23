8af1ca092ece0eaa7fd4a9d3d51d22fc9dda422a
Shannon Watts <100214297+Shannon-Watts@users.noreply.github.com>
2022-06-23 18:50:43 +0100

    most recent changes

--- code/map.js
+++ code/map.js
@@ -14,15 +14,22 @@ var marker = L.marker([50.45, 30.5], {
     draggable: false,
     title: "Kiev"
 }).addTo(myMap);
-marker.binddPopup("Kiev, Ukraine")
+marker.bindPopup("Kiev, Ukraine")
 
-// add in json and add markers to capital cities 
+// add in json 
+//var url = "https://api.acleddata.com/acled/read?key=zHU1Q4Hg0aShRL8YlQEP&email=shannon.sdw.watts@gmail.com&?event_type=Violence&nbsp;against&nbsp;civilians&event_type_where=%3D&event_date=2021-01-01|2022-06-22&event_date_where=BETWEEN"
+var url = "https://api.acleddata.com/acled/read?key=zHU1Q4Hg0aShRL8YlQEP&email=shannon.sdw.watts@gmail.com"
+////d3.json(url).then(function(response) {
 
-// add in json and create 2 layers 
+d3.json("http://127.0.0.1:https://api.acleddata.com/acled/read?key=zHU1Q4Hg0aShRL8YlQEP&email=shannon.sdw.watts@gmail.com").then( data => {
+    console.log(data);
+    // add markers to capital cities 
+});
+// create 2 layers 
 
-// count of event type civilian violence 
+    // count of event type civilian violence 
 
-// count of fatalities 
+    // count of fatalities 
 
 // Layer control 
 // L.control.layers()
