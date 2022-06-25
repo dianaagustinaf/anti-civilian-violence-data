dc2525956cf63ca4ac4dd87bf8a20c58f7abdb3a
Shannon Watts <100214297+Shannon-Watts@users.noreply.github.com>
2022-06-25 08:18:47 +0100

    working on leaflet map

--- code/ukriane_map.js
+++ code/ukriane_map.js
@@ -18,15 +18,31 @@ marker.bindPopup("Kiev, Ukraine")
 
 // add in json 
 
-    // add markers to cities 
-
-    // create 2 layers 
+    // add markers to locations using lat and long 
+    //for(var i=0; i < locations.length; i++) {
+    for(var key in jsondata.("NAME OF LAT & LNG")) {
+        var latLng = L.latLng([jsondata.("NAME OF LAT & LNG")].x, [jsondata.("NAME OF LAT & LNG")y]);
+        L.circles(latLng, {icon: enciv} {
+            fillOpactity: 0.75,
+            // do we want to change the color based on sub event type / fatality count?
+            color = orange, 
+            fillColour: orange,
+            radius: 50 
+            // here I think I need to re loop through to find the corresponding location name to latlong?
+        }).bindPopup(`<h1>${location[i].}</h1> <hr> <h3> WHAT ELSE DO WE WANT HERE?</h3>`).addTo(myMap);
+    
+    // locationlayer 
+    var locations = L.layerGrouup([latLng]);
 
     // count of event type civilian violence 
 
     // count of fatalities 
 
+    // sub event type?
+
     // Layer control 
     
     // L.control.layers()
 
+    
+    }
